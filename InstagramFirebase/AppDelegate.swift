c898ceac5fe450f1e0b100b13f22e9ac5db91471
Dagmawi Nadew <techoffen4@gmail.com>
2018-10-13 23:28:29 -0400

    First Commit

--- InstagramFirebase/AppDelegate.swift
+++ InstagramFirebase/AppDelegate.swift
@@ -13,9 +13,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    fileprivate func setupWindow() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        setupWindow()
         return true
     }
 
